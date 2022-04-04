4834b4589646ec9189ec5b623bc5159906270fc3
KakusakovOffice <99006858+KakusakovOffice@users.noreply.github.com>
2022-04-04 08:01:54 +0300

    Add files via upload

--- cazik v1_0.py
+++ cazik v1_0.py
@@ -1,12 +1,13 @@
 from random import randint 
+from time import sleep
 
 class coef_type:
     # тип коэффициента, например шестерка, валет, и т.п.
     
-    def __init__(self, init_sign: str, init_rel_p: int, init_coef_dict):
+    def __init__(self, init_sign: str, init_rel_p: int, init_mult_dict):
         self.__sign = init_sign  # значек
         self.__rel_p = init_rel_p  # относительная вероятность выпадения
-        self.__coef_dict = init_coef_dict  # словарь, который соединяет количество выпадений с множителем ставки
+        self.__mult_dict = init_mult_dict  # словарь, который соединяет количество выпадений с множителем ставки
     
     def get_sign(self):
         return self.__sign
@@ -14,26 +15,26 @@ class coef_type:
     def get_rel_p(self):
         return self.__rel_p
     
-    def get_coef_dict(self):
-        return self.__coef_dict
+    def get_mult_dict(self):
+        return self.__mult_dict
 
 
 slots = [''] * 5  # выпавшие слоты
 coefs = {  # словарь, содержащий все возможные типы коэффициентов
-    '6' : coef_type('6', 15, { 2 : 0.6, 3 : 6.66 }),
-    '7' : coef_type('7', 13, { 3 : 7.7 }),
-    '8' : coef_type('8', 11, { 2 : 0.8, 3 : 1.1, 4 : 1.1 }),
-    '9' : coef_type('9', 9, { 2 : 0.9, 3 : 1.3, 4 : 1.3, 5 : 4.5 }),
-    '10' : coef_type('10', 7, { 3 : 1.5, 4 : 1.5, 5 : 5.0 }),
-    'J' : coef_type('J', 5, { 2 : 1.1, 3 : 1.7, 4 : 1.7, 5 : 5.5 }),
-    'Q' : coef_type('Q', 4, { 2 : 1.2, 3 : 1.9, 4 : 1.9, 5 : 6.0 }),
-    'K' : coef_type('K', 3, { 2 : 1.3, 3 : 2.1, 4 : 2.1, 5 : 8.0 }),
-    'A' : coef_type('A', 2, { 2 : 2.0, 3 : 8.0, 4 : 8.0, 5 : 10.0 }),
-    'JP' : coef_type('jackpot', 1, { 2 : 10.0, 3 : 20.0, 4 : 50.0, 5 : 100.0 })
+    '6' : coef_type('6', 100, { 2 : 0.6, 3 : 6.66 }),
+    '7' : coef_type('7', 100, { 3 : 7.7 }),
+    '8' : coef_type('8', 160, { 2 : 0.8, 3 : 1.1, 4 : 1.1 }),
+    '9' : coef_type('9', 300, { 2 : 0.9, 3 : 1.3, 4 : 1.3, 5 : 4.5 }),
+    '10' : coef_type('10', 150, { 3 : 1.5, 4 : 1.5, 5 : 5.0 }),
+    'J' : coef_type('J', 100, { 2 : 1.1, 3 : 1.7, 4 : 1.7, 5 : 5.5 }),
+    'Q' : coef_type('Q', 50, { 2 : 1.2, 3 : 1.9, 4 : 1.9, 5 : 6.0 }),
+    'K' : coef_type('K', 25, { 2 : 1.3, 3 : 2.1, 4 : 2.1, 5 : 8.0 }),
+    'A' : coef_type('A', 10, { 2 : 2.0, 3 : 8.0, 4 : 8.0, 5 : 10.0 }),
+    'JP' : coef_type('jackpot', 5, { 2 : 10.0, 3 : 20.0, 4 : 50.0, 5 : 100.0 })
 }
 
 
-def seed_slots(): 
+def randomize_slots(): 
     # заполняет слоты названиями типов коэффициентов в соответвии с их относительной вероятностью
     global coefs, slots 
     max_p: int = 0
@@ -52,34 +53,41 @@ def seed_slots():
 
 
 def print_slots():
+    # выводит на экран информацию о выпавших слотах и о джекпоте (если он выпал)
     global slots, coefs
+    pace: float = 0.75  # время спина одного слота в секундах
     jackpot_counter: int = 0
     
     for coef_key in slots:
         if coef_key == 'JP':
             jackpot_counter += 1
         
-        print(coefs[coef_key].get_sign(), end=' ')
+        sleep(pace)
+        print(coefs[coef_key].get_sign(), end=' ', flush=True)
     
     print('', end='\n')
+    sleep(pace)
     
-    if jackpot_counter >= 2: 
-        if jackpot_counter >= 3: 
-            print( 'SUPER', end=' ') 
-        if jackpot_counter >= 4: 
-            print( 'MEGA', end=' ' ) 
-        if jackpot_counter >= 5: 
-            print( 'HYPER', end=' ' )
-        print( 'JACKPOT' ) 
+    if jackpot_counter > 1: 
+        if jackpot_counter > 2: 
+            print('SUPER', end=' ', flush=True) 
+            sleep(pace)
+        if jackpot_counter > 3: 
+            print('MEGA', end=' ', flush=True) 
+            sleep(pace)
+        if jackpot_counter > 4: 
+            print('HYPER', end=' ', flush=True) 
+            sleep(pace)
+        print('JACKPOT') 
 
 
 def find_bet_multiplier(): 
     # возвращает множитель для ставки в зависимости от значения слотов
     mult: float = 1.0
     global slots, coefs 
-    coefs_amount = { }
+    coefs_amount = { }  # словарь соединяющий ключ к типу коэффицента с количеством его выпадений
     for coef_key in coefs:
-        coefs_amount[coef_key] = 0
+        coefs_amount[coef_key] = 0  # добавляет ключ в словарь
     
     for coef_key in slots:
         coefs_amount[coef_key] += 1
@@ -88,7 +96,7 @@ def find_bet_multiplier():
     for coef_key, amount in coefs_amount.items(): 
         if amount > 1:
             no_combos = False
-            mult_dict = coefs[coef_key].get_coef_dict()
+            mult_dict = coefs[coef_key].get_mult_dict()
             if amount in mult_dict:
                 mult *= mult_dict[amount]
     
@@ -98,14 +106,16 @@ def find_bet_multiplier():
     return mult
 
 
-def new_roll(bet: int):
-    seed_slots()
+def new_spin(bet: int):
+    # иммитирует один спин слотов, возвращает выигрыш
+    randomize_slots()
     print_slots()
     profit: int = int(bet * find_bet_multiplier())
     return profit
 
 
 def start_game(balance: int):
+    # главная функция, запускает игру
     print('777  КАЗИК  777', end='\n\n')
     print('Ваш стартовый баланс: ', balance)
     bet: int
@@ -117,13 +127,13 @@ def start_game(balance: int):
             continue
         
         balance -= bet
-        profit = new_roll(bet)
+        profit = new_spin(bet)
         balance += profit
         print('Вы выиграли ', profit)
         print('Ваш баланс: ', balance)
     print('Вы проиграли все ваши деньги')
 
 
-if __name__ == '__main__':
+if __name__ == '__main__': 
     init_balance = 100000
     start_game(init_balance)
